443d567d9f27a874c741d4ffddf906156705b5aa
EmJey <emjeydev@gmail.com>
2023-06-13 20:55:05 +0330

    add pojo folder and UserInformation model

---
+++ src/main/java/com/ifr/informationformregistration/POJO/UserInformation.java
@@ -0,0 +1,83 @@
+package com.ifr.informationformregistration.POJO;
+
+import java.util.UUID;
+
+// This file is made by EmJey
+// Project: Information Form Registration
+// FileName: UserInformation
+// Date: 2023/06/13
+// Email: emjeydev@gmail.com
+// Github: emjeydev
+
+public class UserInformation {
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private int nationalCode;
+    private int phoneNumber;
+    private String address;
+    private String maritalStatus;
+
+    public UserInformation() {
+        this.id = UUID.randomUUID();
+    }
+
+
+//    Getters And Setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(int nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+}
