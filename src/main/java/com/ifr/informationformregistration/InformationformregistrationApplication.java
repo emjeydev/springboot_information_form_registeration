08bccc8d02535288bded2de3135e513902083d2c
EmJey <emjeydev@gmail.com>
2023-06-13 20:43:22 +0330

    add the project

---
+++ src/main/java/com/ifr/informationformregistration/InformationformregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.ifr.informationformregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationformregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationformregistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifr/informationformregistration/InformationformregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifr.informationformregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationformregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
