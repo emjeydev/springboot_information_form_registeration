00b8c0d13dfdd2f4f6b5d8ccf6ee87d8b679706d
EmJey <emjeydev@gmail.com>
2023-06-13 20:58:17 +0330

    add web folder and UserController file

---
+++ src/main/java/com/ifr/informationformregistration/web/UserController.java
@@ -0,0 +1,15 @@
+package com.ifr.informationformregistration.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// This file is made by EmJey
+// Project: Information Form Registration
+// FileName: UserController
+// Date: 2023/06/13
+// Email: emjeydev@gmail.com
+// Github: emjeydev
+
+@RestController
+public class UserController {
+
+}
